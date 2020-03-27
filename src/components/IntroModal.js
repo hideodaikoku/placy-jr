e7a12b3f79487a3dbb212f082c8a1c3bf860b15f
hideo <neildaikoku@gmail.com>
2020-03-27 10:22:46 +0900

    styling :art:

--- src/components/IntroModal.js
+++ src/components/IntroModal.js
@@ -8,7 +8,7 @@ export default class Modal extends Component{
   
     return (
       <div className={showHideClassName}>
-        <section className="modal-main">
+        <section className="modal-main-a">
           <div className="close" onClick={this.props.handleClose}>&times;</div>
           
           <div className="para">
@@ -24,6 +24,11 @@ export default class Modal extends Component{
               Although every effort has been made to ensure the information is accurate,<br/>
               we will not be liable for any damages resulting from errors.<br/>
               Thank you for your understanding and cooperation
+          </div>
+          <div style={{marginTop:"2rem"}}>
+              店舗に関する記載は、株式会社Placyが行っています。 <br/>
+              記載に当たっては正確に記載するよう努めてはおりますが、仮に誤り等がありましても、<br/>
+              それにより発生した損害について、当社は責任を負いかねますので、ご了知おきください。
           </div>
         </section>
       </div>
