c322bfbd1c724e16f3c7a3a06847418e864f7cc4
hideo <neildaikoku@gmail.com>
2020-04-02 20:50:53 +0900

    responsive front modal :tada:

--- src/components/IntroModal.js
+++ src/components/IntroModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import '../styles/modal.css'
+import '../styles/modal.css';
+import placy from "../assets/images/placy-logo.png"
 
 export default class Modal extends Component{
     
@@ -10,7 +11,6 @@ export default class Modal extends Component{
       <div className={showHideClassName}>
         <section className="modal-main-a">
           <div className="close" onClick={this.props.handleClose}>&times;</div>
-          
           <div className="para">
             <h1 className="title">
             We Are Placy, a company creating an alternative place search engine.
@@ -20,16 +20,18 @@ export default class Modal extends Component{
             </p>
           </div>
           <div>
-              Information about stores is provided by Placy.<br/> 
-              Although every effort has been made to ensure the information is accurate,<br/>
-              we will not be liable for any damages resulting from errors.<br/>
+              Information about stores is provided by Placy.
+              Although every effort has been made to ensure the information is accurate,
+              we will not be liable for any damages resulting from errors.
               Thank you for your understanding and cooperation
           </div>
-          <div style={{marginTop:"2rem"}}>
-              店舗に関する記載は、株式会社Placyが行っています。 <br/>
-              記載に当たっては正確に記載するよう努めてはおりますが、仮に誤り等がありましても、<br/>
+          <br/>
+          <div className="text-jp">
+              店舗に関する記載は、株式会社Placyが行っています。
+              記載に当たっては正確に記載するよう努めてはおりますが、仮に誤り等がありましても、
               それにより発生した損害について、当社は責任を負いかねますので、ご了知おきください。
           </div>
+          <img src={placy} alt="placy" className="placy-logo"></img>
         </section>
       </div>
     );
