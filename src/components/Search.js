856f37bdac1d11c02993a8e24825de0440398e68
Hideo Daikoku <neildaikoku@gmail.com>
2020-06-16 11:43:44 +0900

    public endpoints

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,7 +14,7 @@ import helpText from "../data/helpText.json";
 import logo from "../assets/images/logo.png"
 import placy from "../assets/images/placy-logo.png"
 const local_endpoint = 'http://localhost:8000';
-const public_endpoint = 'https://boiling-garden-91117.herokuapp.com';
+const public_endpoint = 'https://master.df7pdxc0x15qw.amplifyapp.com';
 
 
 const rate_limit = 10;
