00731e9e5f87cbcbf5e7280dd6f335b0d603dd74
Hideo Daikoku <neildaikoku@gmail.com>
2020-07-05 01:32:10 +0900

    result page styling changes

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ResultPageStyles from '../styles/resultpage.module.css';
+import ResultPageStyles from '../styles/resultpage.module.scss';
 import stationData from '../data/stations.json';
 import venueData from '../data/venue-info.json';
 import pngMap from "../data/pngMap.json";
@@ -42,7 +42,10 @@ export default class ResultPage extends Component {
         // the place_ids b/w recommender and front end data do not match exactly - they will however, be in the same station
 
         const place = venueData.filter(venue=>venue.place_id===this.props.code);
-        const station_id = place[0].station_id;
+        var station_id = 8
+        if(place[0]){
+            station_id = place[0].station_id;
+        }
         const station = stationData.filter(station=>station.station_id===station_id)[0];
         const bars = venueData.filter(venue=>venue.station_id===station_id)
         this.setState({
@@ -80,55 +83,59 @@ export default class ResultPage extends Component {
                     <Loading/>
                     : null
                     }
-                    <div className={ResultPageStyles.topContainer}>
-                        <h1 id={ResultPageStyles.arrow}
-                            onClick={this.refresh}>
-                                &#8592;
-                        </h1>
-                        <h1 id={ResultPageStyles.title}>{this.state.station_name}</h1>
-                    </div>
-                    <p>{this.state.desc}</p>
-                    <a href={this.state.spotify_link} target="_blank" rel="noopener noreferrer" className={ResultPageStyles.playlist}>
-                        LISTEN ON SPOTIFY
-                    </a>
-                    {
-                    
-                    this.state.bars.map((bar,idx)=>(
-                        
-                        <div key={bar.place_id} className={ResultPageStyles.barContainer}>
-                            
-                            <div className={ResultPageStyles.barTitle}>
-                                <p className={ResultPageStyles.number}>{idx+1}</p>
-                                <p className={ResultPageStyles.clubName}>{bar.club_name_en}</p>
-                            </div>
-                            
-                            
-                            <div className={ResultPageStyles.barImage}>
-                            
-                                <img src={image_hosting_endpoint+bar.place_id+'.png'} alt={bar.clubName}/>
-                           
-                            </div>
-                            
-                            <ul>
-                                <li>{bar.category}</li>
-                            </ul>
-                            
-                            <p className={ResultPageStyles.en}>{bar.description_en}</p>
-                            
-                            <p className={ResultPageStyles.ja}>{bar.description_ja}</p>
-
-                            <a href={bar.google_map_url} target="blank" rel="noopener noreferrer" className={ResultPageStyles.addressLink}>
-                                <small className={ResultPageStyles.address}>{bar.address}</small><br/>
-                            </a>
+                    <div className={ResultPageStyles.sidebar}>
+                        <div className={ResultPageStyles.topContainer}>
+                            <h1 id={ResultPageStyles.arrow}
+                                onClick={this.refresh}>
+                                    &#8592;
+                            </h1>
+                            <h1 id={ResultPageStyles.title}>{this.state.station_name}</h1>
                         </div>
-                    ))}
-                </div>
-                <div className={ResultPageStyles.imgContainer}>
-                    <img 
-                    src={svg_hosting_endpoint+pngMap[this.state.station_id]} 
-                    alt={this.state.station_name}
-                    className={ResultPageStyles.image}
-                    />
+                        <p className={ResultPageStyles.desc}>
+                            {this.state.desc}
+                        </p>
+                        <a href={this.state.spotify_link} target="_blank" rel="noopener noreferrer" className={ResultPageStyles.playlist}>
+                            LISTEN ON SPOTIFY
+                        </a>
+                        {
+                        
+                        this.state.bars.map((bar,idx)=>(
+                            
+                            <div key={bar.place_id} className={ResultPageStyles.barContainer}>
+                                
+                                <div className={ResultPageStyles.barTitle}>
+                                    <span className={ResultPageStyles.number}>{idx+1}</span>
+                                    <span className={ResultPageStyles.clubName}>{bar.club_name_en}</span>
+                                </div>
+                                
+                                
+                                <div className={ResultPageStyles.barImage}>
+                                
+                                    <img src={image_hosting_endpoint+bar.place_id+'.png'} alt={bar.clubName}/>
+                            
+                                </div>
+                                
+                                <ul className={ResultPageStyles.unorderedList}>
+                                    <li className={ResultPageStyles.listItem}>{bar.category}</li>
+                                </ul>
+                                
+                                <p className={ResultPageStyles.en}>{bar.description_en}</p>
+                                
+                                <p className={ResultPageStyles.ja}>{bar.description_ja}</p>
+
+                                <a href={bar.google_map_url} target="blank" rel="noopener noreferrer" className={ResultPageStyles.addressLink}>
+                                    <small className={ResultPageStyles.address}>{bar.address}</small><br/>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={ResultPageStyles.imgContainer}>
+                        <img 
+                        src={svg_hosting_endpoint+pngMap[this.state.station_id]} 
+                        alt={this.state.station_name}
+                        className={ResultPageStyles.image}
+                        />
+                    </div>
                 </div>
                 <Modal 
                     show={this.state.show} 
